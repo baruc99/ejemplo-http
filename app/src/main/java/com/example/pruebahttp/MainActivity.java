da7b407135a7ea068ed98bd810a8ec622a0760f8
BARUC ALBERTO GOMEZ SANCHEZ <bagomez@veracruz.gob.mx>
2023-02-16 10:46:06 -0600

    ejemplo http

---
+++ app/src/main/java/com/example/pruebahttp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.pruebahttp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.textViewResult);
+        //funcion http
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://jsonplaceholder.typicode.com/posts";
+
+        Request request = new  Request.Builder()
+                .url( url )
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if( response.isSuccessful() ){
+                    String myResponse = response.body().string();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextViewResult.setText( myResponse );
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
